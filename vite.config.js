2aab318a9df9879ae9f3a010eddbd41ac9dab791
Tsegaye R <tsegayeroman@gmail.com>
2024-09-08 16:48:01 +0300

    Base

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     port: 1234,
+//     base: "/Netflix-Clone-Sept-2024/", // Update with your repository name
+//   },
+// });
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/Netflix-Clone-Sept-2024/", // Set the correct base path
   server: {
-    port: 1234,
-    base: "/Netflix-Clone-Sept-2024/", // Update with your repository name
+    port: 1234, // Local development port
   },
 });
 
